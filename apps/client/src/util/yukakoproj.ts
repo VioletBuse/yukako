8bae5f39f1341a6e545d6d1df5936b524ccc750f
JulianBuse <44533409+JulianBuse@users.noreply.github.com>
2024-01-19 17:56:16 +0100

    added bindings to read projectfile

--- apps/client/src/util/yukakoproj.ts
+++ apps/client/src/util/yukakoproj.ts
@@ -20,22 +20,112 @@ const configFileSchema = z.object({
             id: z.string(),
         }),
     ),
+
+    text_bindings: z
+        .array(
+            z.union([
+                z.object({
+                    name: z.string(),
+                    value: z.string(),
+                }),
+                z.object({
+                    name: z.string(),
+                    file: z.string(),
+                }),
+            ]),
+        )
+        .optional(),
+
+    json_bindings: z
+        .array(
+            z.union([
+                z.object({
+                    name: z.string(),
+                    value: z.union([
+                        z.string(),
+                        z.number(),
+                        z.boolean(),
+                        z.null(),
+                        z.array(z.any()),
+                        z.record(z.any()),
+                    ]),
+                }),
+                z.object({
+                    name: z.string(),
+                    file: z.string(),
+                }),
+            ]),
+        )
+        .optional(),
+
+    binary_bindings: z
+        .array(
+            z.union([
+                z.object({
+                    name: z.string(),
+                    base64: z.string(),
+                }),
+                z.object({
+                    name: z.string(),
+                    file: z.string(),
+                }),
+            ]),
+        )
+        .optional(),
 });
+
+type ProjectRoute = {
+    host: string;
+    paths: string[];
+};
+
+type ProjectDeployment = {
+    server: string;
+    name: string;
+    id: string;
+};
+
+type ProjectTextBinding =
+    | {
+          name: string;
+          value: string;
+      }
+    | {
+          name: string;
+          file: string;
+      };
+
+type ProjectJsonBinding =
+    | {
+          name: string;
+          value: any;
+      }
+    | {
+          name: string;
+          file: string;
+      };
+
+type ProjectBinaryBinding =
+    | {
+          name: string;
+          base64: string;
+      }
+    | {
+          name: string;
+          file: string;
+      };
 
 type Project = {
     folder: string;
     entrypoint: string;
 
-    routes: {
-        host: string;
-        paths: string[];
-    }[];
+    routes: ProjectRoute[];
 
-    deployments: {
-        server: string;
-        name: string;
-        id: string;
-    }[];
+    deployments: ProjectDeployment[];
+
+    text_bindings: ProjectTextBinding[];
+    json_bindings: ProjectJsonBinding[];
+    binary_bindings: ProjectBinaryBinding[];
 };
 
 export const readProjectFile = (): Project => {
@@ -54,5 +144,23 @@ export const readProjectFile = (): Project => {
         throw parseResult.error;
     }
 
-    return parseResult.data;
+    const routes: ProjectRoute[] = parseResult.data.routes ?? [];
+
+    const deployments: ProjectDeployment[] = parseResult.data.deployments ?? [];
+
+    const text_bindings: ProjectTextBinding[] =
+        parseResult.data.text_bindings ?? [];
+    const json_bindings: ProjectJsonBinding[] =
+        parseResult.data.json_bindings ?? [];
+    const binary_bindings: ProjectBinaryBinding[] =
+        parseResult.data.binary_bindings ?? [];
+
+    return {
+        folder: dir,
+        entrypoint: parseResult.data.entrypoint,
+
+        routes,
+
+        deployments,
+    };
 };
