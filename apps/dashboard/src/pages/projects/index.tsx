e6c687b580e2f852fc06ee1e4cfb719e20895a94
JulianBuse <44533409+JulianBuse@users.noreply.github.com>
2024-02-07 14:17:15 +0100

    added loading and error states to the projects list page

--- apps/dashboard/src/pages/projects/index.tsx
+++ apps/dashboard/src/pages/projects/index.tsx
@@ -8,8 +8,18 @@ import {
     CardTitle,
 } from '@/components/ui/card';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { Server, Terminal } from 'lucide-react';
+import { AlertCircle, Server, Terminal } from 'lucide-react';
 import { Link } from 'wouter';
+import { Skeleton } from '@/components/ui/skeleton';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
 
 export const ProjectsPage: React.FC = () => {
     const [projects, fetchProjectsError, loadingProjects] = useListProjects();
@@ -19,15 +29,44 @@ export const ProjectsPage: React.FC = () => {
     return (
         <>
             <MainLayout selectedTab='projects'>
-                <h1>Projects</h1>
+                <h1 className='text-3xl font-medium mb-2'>Projects</h1>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button className='mb-2'>Create New Project</Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Create New Project</DialogTitle>
+                            <DialogDescription>
+                                You can create a new project here, or use
+                                &#96;yukactl projects create&#96; from the CLI.
+                            </DialogDescription>
+                        </DialogHeader>
+                    </DialogContent>
+                </Dialog>
+                {fetchProjectsError && (
+                    <Alert variant='destructive' className='mb-2'>
+                        <AlertCircle className='h-4 w-4' />
+                        <AlertTitle>Error</AlertTitle>
+                        <AlertDescription>
+                            {fetchProjectsError ?? 'An unknown error occurred.'}
+                        </AlertDescription>
+                    </Alert>
+                )}
                 <div className='grid gap-2 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 xl2:grid-cols-4 xl3:grid-cols-5'>
+                    {loadingProjects &&
+                        Array.from({ length: 5 }).map((_, i) => (
+                            <Skeleton className='w-full h-52' key={i} />
+                        ))}
                     {projects?.map((project) => (
                         <Link href={`/projects/${project.id}`} key={project.id}>
                             <Card
-                                className='hover:bg-accent transition-all duration-200 ease-in-out cursor-pointer'
+                                className='hover:bg-accent transition-all duration-200 ease-in-out cursor-pointer '
                                 key={project.id}>
-                                <CardHeader>
-                                    <CardTitle>{project.name}</CardTitle>
+                                <CardHeader className='space-y-1 pb-2'>
+                                    <CardTitle className='text-lg'>
+                                        {project.name}
+                                    </CardTitle>
                                     <CardDescription>
                                         Project Created{' '}
                                         {new Date(
@@ -41,7 +80,7 @@ export const ProjectsPage: React.FC = () => {
                                             <Alert>
                                                 <Server className='h-4 w-4' />
                                                 <AlertTitle>
-                                                    Last Deployed: Version{' '}
+                                                    Version{' '}
                                                     {
                                                         project.latest_version
                                                             .version
