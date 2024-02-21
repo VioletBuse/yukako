f4b7a2b8bc9c09cd4eaa15abe17d047327a6cdb6
JulianBuse <44533409+JulianBuse@users.noreply.github.com>
2024-02-21 10:38:17 +0100

    added options to serve function for sites

--- packages/extensions/extensions/sites/index.ts
+++ packages/extensions/extensions/sites/index.ts
@@ -3,33 +3,68 @@ import mime from 'mime';
 export type SiteBindingEnv = {} & Record<string, ArrayBuffer>;
 
 export type SiteNamespace = {
-    serve: (input: Request | string) => Response;
+    serve: (
+        input: Request | string,
+        opts?: {
+            rewriteURL?: (url: URL) => URL;
+            ifNoExtensionAddHtml?: boolean;
+            rewriteBasePathToIndexHtml?: boolean;
+            defaultPath?: string;
+        },
+    ) => Response;
 };
 
 const makeSiteBinding = (env: SiteBindingEnv): SiteNamespace => {
     return {
-        serve: (req) => {
-            let path =
-                typeof req === 'string' ? req : new URL(req.url).pathname;
+        serve: (req, opts) => {
+            let url =
+                typeof req === 'string'
+                    ? new URL(
+                          req,
+                          req.startsWith('/') ? 'https://dummy.com' : undefined,
+                      )
+                    : new URL(req.url);
 
-            if (path === '/') {
-                path = '/index.html';
+            if (opts?.rewriteURL) {
+                url = opts.rewriteURL(url);
             }
 
-            const index = env[path];
-            // console.log('typeof index', typeof index);
-            // console.log('instanceof ArrayBuffer', index instanceof ArrayBuffer);
+            if (
+                opts?.rewriteBasePathToIndexHtml !== false &&
+                url.pathname === '/'
+            ) {
+                url.pathname = '/index.html';
+            }
 
-            const mimetype = mime.getType(path) || 'text/plain';
+            const filename = url.pathname.split('/').pop();
+            const extension = filename?.split('.').pop();
 
-            const response = new Response(index, {
+            if (opts?.ifNoExtensionAddHtml && !extension) {
+                url.pathname += '.html';
+            }
+
+            let file = env[url.pathname];
+
+            if (!file) {
+                if (opts?.defaultPath) {
+                    file = env[opts.defaultPath];
+                }
+
+                if (!file) {
+                    return new Response('Not Found', {
+                        status: 404,
+                    });
+                }
+            }
+
+            const mimetype = mime.getType(url.pathname) || 'text/plain';
+
+            return new Response(file, {
                 status: 200,
                 headers: {
                     'content-type': mimetype,
                 },
             });
-
-            return response;
         },
     };
 };
