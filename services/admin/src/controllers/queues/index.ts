4be2858915262c3e3345b15f75038b31cfbedf5f
JulianBuse <44533409+JulianBuse@users.noreply.github.com>
2024-03-20 19:46:55 +0100

    created GET /api/queues/:id handler

--- services/admin/src/controllers/queues/index.ts
+++ services/admin/src/controllers/queues/index.ts
@@ -6,6 +6,7 @@ import { authenticate } from '../../lib/authenticate';
 import { queues } from '@yukako/state/src/db/schema';
 import { nanoid } from 'nanoid';
 import { QueueQueueDataResponseBodyType } from '@yukako/types/src/admin-api/queues';
+import { eq } from 'drizzle-orm';
 
 const queuesRouter = Router();
 
@@ -39,6 +40,40 @@ queuesRouter.post('/', async (req, res) => {
         };
 
         respond.status(201).message(data).throw();
+    } catch (err) {
+        respond.rethrow(err);
+
+        if (err instanceof ZodError) {
+            respond.status(400).message('Bad Request').throw();
+        }
+
+        respond.status(500).message('Internal Server Error').throw();
+        throw err;
+    }
+});
+
+queuesRouter.get('/:id', async (req, res) => {
+    try {
+        const db = getDatabase();
+
+        await authenticate(req);
+
+        const id = req.params.id;
+
+        const result = await db.query.queues.findFirst({
+            where: eq(queues.id, id),
+        });
+
+        if (!result) {
+            respond.status(404).message('Not Found').throw();
+        }
+
+        const data: QueueQueueDataResponseBodyType = {
+            id: result.id,
+            name: result.name,
+        };
+
+        respond.status(200).message(data).throw();
     } catch (err) {
         respond.rethrow(err);
 
